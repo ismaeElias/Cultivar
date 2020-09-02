7f7bdecaebf5a8ee0c590b64db90a800838aadfb
Ismael Elias <51543534+ismaeElias@users.noreply.github.com>
2020-09-01 22:32:41 -0300

    implementando menu

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+
+  isCheckTemperatura: boolean = false;
+  isCheckIluminacao: boolean = false;
+  isCheckIrrigacao: boolean = false;
+
+  constructor(private bluetoothSerial: BluetoothSerial, private alertController: AlertController) { }
 
   ngOnInit() {
+    /* this.alertSincronizacao()*/
   }
 
+  checkTemperatura(event) {
+    if (event.checked) {
+      this.bluetoothSerial.write('LTE');
+    } else {
+      this.bluetoothSerial.write('DTE');
+    }
+  }
+
+  checkIrrigacao(event) {
+    if (event.checked) {
+      this.bluetoothSerial.write('LIR');
+    } else {
+      this.bluetoothSerial.write('DIR');
+    }
+  }
+
+  checkIluminacao(event) {
+    if (event.checked) {
+      this.bluetoothSerial.write('LIL');
+    } else {
+      this.bluetoothSerial.write('DIL');
+    }
+  }
+
+  async alertSincronizacao() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: 'Deseja sincronizar os dados ?',
+      buttons: [
+        {
+          text: 'Confirmar',
+          role: 'confirmar',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Cancelar',
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  sendBluetooth() {
+    this.bluetoothSerial.write('Funcionou garai!');
+    alert('Clicou');
+  }
 }
